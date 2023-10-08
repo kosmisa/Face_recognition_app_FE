123ee897004d502b1427ea8263e5acc7e21d5004
kosmisa <vukasin.jevtic@gmail.com>
2023-10-08 13:58:26 +0200

    test2

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onSubmitRegister = () => {
-    fetch("http://localhost:3000/register", {
+    fetch("https://worrisome-rose-dalmatian.cyclic.app/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
